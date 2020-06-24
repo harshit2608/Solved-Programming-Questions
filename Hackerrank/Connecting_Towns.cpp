fdafafc7ac23fe47165ecaa14047943017f3d437
Harshit Bargujar <harshitrock111@gmail.com>
2020-06-25 00:46:55 +0530

    Day 4

--- Hackerrank/Connecting_Towns.cpp
+++ Hackerrank/Connecting_Towns.cpp
@@ -8,12 +8,15 @@ int main()
     cin >> t;
     while (t--)
     {
+        long long int ans = 1;
+        long long int x;
         cin >> n;
         for (int i = 0; i < n - 1; i++)
         {
-            long long x;
             cin >> x;
-            
+            ans = (ans * x) % 1234567;
         }
+        cout << ans << endl;
     }
+    return 0;
 }
