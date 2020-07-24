b93761155213a9f78a068a95bbee4a151f47e3b8
Harshit Bargujar <harshitrock111@gmail.com>
2020-07-24 21:20:07 +0530

    Solutions

---
+++ CodingNinjas/Tell the positions.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define MAX (int(1e5))
+#define ll long int
+
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    int m1, m2, m3;
+    multimap<ll, string, std::greater<ll>> ans;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s >> m1 >> m2 >> m3;
+        int sum = m1 + m2 + m3;
+        ans.insert(make_pair(sum, s));
+    }
+
+    int count = 1;
+    for (auto &i : ans)
+    {
+        cout << count << " " << i.second << endl;
+        count++;
+    }
+    return 0;
+}
---
+++ GeeksForGeeks/Easy/Nth number made of prime digits.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+#define MAX (int(1e5))
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int count = 0;
+        for (int i = 0; i <= MAX;i++)
+        {
+            int ans = 0;
+            if()
+            {
+                count++;
+                if(count == n)
+                {
+                    cout << ans;
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
